b8ac00017acee37be560bef8c50ac5b122edcf25
mdzwink <mdzwink@gmail.com>
2022-05-17 08:41:53 -0400

    adding _header.ejs and urls_new.ejs. Working on implementing a placeholder response to post request from urls_new form

--- express_server.js
+++ express_server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+const PORT = 8080;
 const log = console.log;
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   "9sm5xK": "http://www.google.com"
+
 };
+
+function generateRandomString() {
+  let randomString = '';
+  for (let i = 0; i <= 6; i++) {
+    randomString += Math.floor(Math.random() * 10)
+  }
+}
 
 
 app.get('/', (req, res) => {
@@ -22,13 +32,29 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World!.!</b></body></html>\n")
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get("/urls.json", (req, res) => {
-  log('json is here')
   res.json(urlDatabase);
 });
+
+
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase  };
   res.render('urls_index', templateVars);
+})
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+})
+
+
+app.post('/urls', (req, res) => {
+  log(req.body);
+  res.send('Ok');
 })
 
 app.listen(PORT, () => {
