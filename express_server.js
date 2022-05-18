38ad2929bda21e267c25aa3fd0bbd675072c4d07
mdzwink <mdzwink@gmail.com>
2022-05-18 17:44:45 -0400

    small updates

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 app.set('view engine', 'ejs');
+// const morgan = require('morgan');// Doesn't work with nodemon??
+// app.use(morgan(dev));
 const PORT = 8080;
 const log = console.log;
 
@@ -11,7 +15,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
   
 };
-
+//----username---
+let username;
+//----username---
 function generateRandomString() {
   let randomString = '';
   for (let i = 0; i < 6; i++) {
@@ -20,17 +26,45 @@ function generateRandomString() {
   return randomString;
 }
 
+app.post('/urls/:shortURL/update', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[req.params.shortURL]
+  urlDatabase[shortURL] = req.body.longURL;
+  log(shortURL);
+  log('updated longURL')
+  res.redirect('/urls');
+})
 
+app.get('/urls/:shortURL/edit', (req, res) => {//<<<<<<<<<<<<
+  const templateVars = { username: username, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+})
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
-  console.log(shortURL)
   delete urlDatabase[shortURL];
+  log(shortURL)
   log('item deleted')
   res.redirect('/urls');
 })
+
+app.post('/login', (req, res) => {
+  username = req.body.username;
+  log('Cookies: ', res.cookie('username', username))
+
+  res.redirect('/urls')
+})
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  username = undefined;
+  res.redirect('/urls')
+})
+
+
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  templateVars = { username: username };
+  res.render('urls_new', templateVars);
 })
 
 
@@ -58,7 +92,7 @@ app.post('/urls', (req, res) => {
 
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase  };
+  const templateVars = { username: username, urls: urlDatabase  };
   res.render('urls_index', templateVars);
 })
 
